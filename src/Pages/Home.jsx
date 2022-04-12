b76345c0b3c64e48381fb4edcf41b3d66092f742
moretza rezaeian <morteza78rezaeian@gmail.com>
2022-04-12 22:19:31 +0430

    header done! and color and font set

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import Hero from '../Components/Hero'
 import Header from '../Layout/Header'
 
 const Home = () => {
   return (
     <>
     <Header />
+    <Hero />
     </>
   )
 }
